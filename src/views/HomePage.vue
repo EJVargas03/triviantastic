ec0b632a7e077ab256ca8ee86e3463732d038a9d
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-10-11 22:18:35 -0500

    Part 1 of 3 complete

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,13 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import useAPI from '@/composables/useAPI'
+
+const api = useAPI()
+const categories = ref([])
+
+onMounted(async () => {
+    categories.value = await api.getCategories()
+})
 </script>
 
 <template>
@@ -8,9 +17,14 @@
         <img class="logo" src="logo.svg" alt="logo" />
     </div>
     <div class="categories">
-        <RouterLink v-for="n in 24" :key="n" :to="`/question/category/${n}`" class="category">Category -  {{ n }}
+        <RouterLink v-for="category in categories" 
+            :key="category.id" 
+            :to="`/question/category/${category.id}`"
+            class="category">
+            {{ category.name }}
         </RouterLink>
      </div>
+     
 </template>
 
 <style lang="postcss" scoped>
@@ -32,4 +46,6 @@
     }
   }
 }
+
+
 </style>
